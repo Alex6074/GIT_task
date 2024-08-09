fb6bb828deef39c743d3fd0aa36f71b33b8027f2
Alex6074 <lpardaev607@gmail.com>
2024-08-09 13:13:48 +0300

    changes in feature 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Some changes");
         System.out.println("Changes feature 1");
         System.out.println("Changes feature 1");
+        System.out.println("Changes feature 1");
     }
 }
